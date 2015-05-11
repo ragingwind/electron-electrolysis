8e35eabfc6cd98cfa3a5de18f5710d862cb61f3c
ragingwind <ragingwind@gmail.com>
2015-05-11 13:15:52 +0900

    Fix intent

--- electrolysis/index.js
+++ electrolysis/index.js
@@ -9,25 +9,25 @@ require('crash-reporter').start();
 let mainWindow = null;
 
 app.on('window-all-closed', function () {
-	if (process.platform !== 'darwin') {
-		app.quit();
-	}
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
 });
 
 app.on('ready', function () {
-	mainWindow = new BrowserWindow({
-		width: 1200,
-		height: 600,
-		resizable: false
-	});
+  mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 600,
+    resizable: false
+  });
 
   // WARN: Inspect window should be opened before loading URL
   mainWindow.inspectElement(0, 0);
 
-	mainWindow.loadUrl(`file://${__dirname}/index.html`);
-	mainWindow.on('closed', function () {
-		// deref the window
-		// for multiple windows store them in an array
-		mainWindow = null;
-	});
+  mainWindow.loadUrl(`file://${__dirname}/index.html`);
+  mainWindow.on('closed', function () {
+    // deref the window
+    // for multiple windows store them in an array
+    mainWindow = null;
+  });
 });
